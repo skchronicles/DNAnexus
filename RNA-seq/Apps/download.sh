9689c05c768b659447026fec35afe42bc2fb51d5
skchronicles <kuhnsa2@vcu.edu>
2019-02-08 11:48:30 -0500

    RNA-seq pipeline pre-optimization

---
+++ RNA-seq/Apps/download.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Assumes dx toolkit is in $PATH
+dx get app-ccbr_fastqc_array/0.0.1
+dx get app-ccbr_cutadapt_array_pe/0.0.1
+dx get app-ccbr_fastqc_array/0.0.1
+dx get app-ccbr_fastqscreen_db1_array_v1/0.0.2
+dx get app-ccbr_fastqscreen_db2_array/0.0.2
+dx get app-ccbr_star_p1/0.0.1
+dx get app-ccbr_star_p2/0.0.1
+dx get app-ccbr_bam2rsemcounts/0.0.1
+dx get app-ccbr_rsem_results_merge/0.0.2
+dx get app-ccbr_rseqc_array/0.0.2
+dx get app-ccbr_rnaseq_multiqc/0.0.1
